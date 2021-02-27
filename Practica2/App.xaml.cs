1db97ce84f2b89f74625d58f349fbd994d05a94f
rafaelm71 <rafael00003@gmail.com>
2021-02-26 22:48:20 -0400

    Code

---
+++ Practica2/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Practica2.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GridUI();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Practica2/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Practica2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practica2.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new User();
+        }
+    }
+}
